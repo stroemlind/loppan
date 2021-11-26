9f5d6bb9472f48fb5b2e3f30414f25416928d939
stroemlind <elin.stromlind@gmail.com>
2021-11-26 16:32:49 +0000

    Import functions and run.py code

--- run.py
+++ run.py
@@ -1,15 +1,17 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
-
 # Lists for the game functions
 import random
+import time
+import os
+import sys
+from os import system, name
 
-# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+def clear():
+    os.system("cls" if os.name == "nt" else "clear")
+
+
 color_list_one = ['red', 'green', 'orange', 'pink']
 color_list_two = ['blue', 'yellow', 'purple', 'brown']
-
-print('Welcome to the game of "Loppan".\n')
 
 
 def get_player_num():
@@ -19,6 +21,7 @@ def get_player_num():
     that must be a number between 1 to 10. The loop will request
     data until the user input is valid.
     """
+    clear()
     print("""
 Loppan will test your mathematical skills.
 Follow the instruction provided. Let's start!
@@ -34,15 +37,19 @@ Follow the instruction provided. Let's start!
             player_num = int(get_num)
 
             if player_num > 0 and player_num < 11:
-                print(f'Your choice is {player_num}')
+                clear()
+                print(f'Your choice is {player_num}\n')
                 number.append(player_num)
                 break
             else:
                 print('Please choose a valid number.')
+                time.sleep(1.5)
                 continue
         except ValueError:
             print('Not a number: please try again.\n')
+            time.sleep(1.5)
             continue
+    time.sleep(1.5)
     get_color_list(player_num)
 
 
@@ -52,13 +59,17 @@ def get_color_list(player_num):
     Check if number input value is an even or odd number,
     print differnet list depending on number.
     """
+    print('Choose a color:')
+
     if player_num % 2 == 0:
-        print('Choose a color:')
-        print(color_list_one)
+        for color in color_list_one:
+            print(color, end=' ')
+        time.sleep(1.5)
         get_color(color_list_one)
     else:
-        print('Choose a color:')
-        print(color_list_two)
+        for color in color_list_two:
+            print(color, end=' ')
+        time.sleep(1.5)
         get_color(color_list_two)
 
 
@@ -70,12 +81,14 @@ def get_color(color_list):
     """
     while True:
         try:
-            player_color = input('Enter your color choice:\n')
+            player_color = input('\n\nEnter your color choice:\n')
             if player_color in color_list:
                 for index, color in enumerate(color_list):
                     if color == player_color:
+                        clear()
                         print(f'Your choice is {player_color}.\n')
                         math_quest(index)
+                        break
                 break
             else:
                 print(f'{player_color} is not a valid color, try again.\n')
@@ -112,7 +125,6 @@ def math_quest(index):
         answer = int(first/second)
 
     print('What is ', first, operator[index], second, '?')
-    # return answer
     get_answer(answer)
 
 
@@ -123,7 +135,7 @@ def get_answer(answer):
     Check if answer provided is correct or incorrect.
     """
     player_answer = int(input('Enter your answer here: '))
-
+    clear()
     if player_answer == answer:
         print(f'Hooray! {answer} is the right answer\n')
     else:
@@ -151,14 +163,17 @@ def about_game():
     cootie catcher, chatterbox, salt cellar, whirlybird, or paku-paku.
     Loppan is a form of origami and made out of paper. The loppa are labeled
     with colors and/or numbers.
+
     These serve as an option for the player to choose. Inside each label,
     there is a message or a task for the player to execute.
+
     The person operating the loppa manipulates the device based on the choices
     made by the player, often folding the loppa a certain amount of time
     and then revealing one of the hidden messages chosen by the player.
     The game originates from Japan. The earliest record of using the Loppa in
     the western world is from 1836. If you would like to know more,
     you can visit these websites:
+
     http://www.origamiheaven.com/historyofthesaltcellar.htm
     https://en.wikipedia.org/wiki/Paper_fortune_teller
     """)
@@ -180,25 +195,37 @@ def create_own():
     To make a Loppa/fortune teller, you need:
         * a piece of A4 paper or a squared shape paper
         * scissor
-        * pencils
+        * pencils""")
 
+    input('\nPress Enter to proceed')
+
+    print("""
     Instructions:
     1. Fold the corners of a sheet of paper so they meet up
         on the opposite sides.
         If the paper is not a square, cut the top off,
-        making it a square sheet with diagonal creases.
+        making it a square sheet with diagonal creases.""")
 
+    input('\nPress Enter to proceed')
+
+    print("""
     2. Fold the four corners of the square into the center,
         forming a shape known in origami terminology as a
         blintz base or cushion fold.
         Turn the paper square over,
-        and fold the four corners into the middle a second time.
+        and fold the four corners into the middle a second time.""")
 
+    input('\nPress Enter to proceed')
+
+    print("""
     3. The top of the square should look like a squared cake with 8 slices.
         On each slice, you can put a number or a color.
         Open up one of the corners at a time and write a fortune
-        or a task under each slice.
+        or a task under each slice.""")
 
+    input('\nPress Enter to proceed')
+
+    print("""
     4. When done, it should look like a folded cushion,
         as at the end of step 2.
         Work your fingers into the pockets on the backside of the cushion,
@@ -210,6 +237,7 @@ def exit_game():
     """
     The user will exit the game
     """
+    clear()
     print('Exit Game \n')
 
 
@@ -217,14 +245,17 @@ def main():
     """
     The main function of the program.
     """
+    clear()
     while True:
+        print('Welcome to the game of "Loppan".\n')
         print_menu()
         option = ''
         try:
-            option = int(input('Where do you want to go? Enter number:\n'))
+            option = int(input('\nWhere do you want to go? Enter number:\n'))
         except ValueError as e:
             print(f'Invalid data: {e}, try again.\n')
 
+        clear()
         if option == 1:
             about_game()
         elif option == 2:
@@ -237,7 +268,8 @@ def main():
         else:
             print('Please chose a number between 0 and 3')
 
-    print('Thank you for visiting "Loppan". Please comeback again')
+    print('Thank you for visiting "Loppan". Please come back again')
+    sys.exit()
 
 
 main()
